e1e1a1d75b776744182053bfb262b2c4c5094b66
Huynh Ngoc Qui <quichua333@gmail.com>
2023-11-22 11:28:05 +0700

    .

--- shop-app-client/src/components/Layout/LayoutNoSidebar/Header/index.js
+++ shop-app-client/src/components/Layout/LayoutNoSidebar/Header/index.js
@@ -13,79 +13,79 @@ function Header() {
     const navigate = useNavigate();
     const [indexCategoryActive, setIndexCategoryActive] = useState(-1);
     const [showDetailPopUp, setShowDetailPopup] = useState(false)
-    const [valueSearch,setValueSearch] = useState('');
+    const [valueSearch, setValueSearch] = useState('');
     const categories = [
         {
             category: 'Áo',
             listTypes: [
-                {   
+                {
                     name: 'Áo khoác',
                     link: `/collection/1`
                 }
                 ,
-                {   
+                {
                     name: 'Áo mùa đông',
                     link: `/collection/1`
                 },
-                {   
+                {
                     name: 'Áo thể thao',
                     link: `/collection/1`
                 },
-                {   
+                {
                     name: 'Áo tết',
                     link: `/collection/1`
                 }
             ],
             images: [
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2023/GRAPHICS.AGANIN.10_10.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2023/GRAPHICS.AGANIN.10_10.jpg',
                     alt: 'Phê pha',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
                     alt: 'Yasuo áo rách',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
                     alt: 'Leesin mù',
                 }
-               
-                
-                
+
+
+
             ]
         },
         {
             category: 'Quần',
             listTypes: [
-            {   
-                name: 'Quần dài',
-                link: `/collection/1`
-            }
-            ,
-            {   
-                name: 'Quần short',
-                link: `/collection/1`
-            },
-            {   
-                name: 'Quần thể thao',
-                link: `/collection/1`
-            },
-            {   
-                name: 'Quần báo',
-                link: `/collection/1`
-            }
-        ],
+                {
+                    name: 'Quần dài',
+                    link: `/collection/1`
+                }
+                ,
+                {
+                    name: 'Quần short',
+                    link: `/collection/1`
+                },
+                {
+                    name: 'Quần thể thao',
+                    link: `/collection/1`
+                },
+                {
+                    name: 'Quần báo',
+                    link: `/collection/1`
+                }
+            ],
             images: [
                 {
                     src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2023/GRAPHICS.AGANIN.10_10.jpg',
                     alt: 'Jeans Copper Denim',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
                     alt: 'Quần lửng',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
                     alt: 'Quần dài',
                 }
 
@@ -94,16 +94,16 @@ function Header() {
         {
             category: 'Đồ lót',
             listTypes: [
-                {   
+                {
                     name: 'Brief (Tam giác)',
                     link: `/collection/1`
                 }
                 ,
-                {   
+                {
                     name: 'Trunk (Boxer)',
                     link: `/collection/1`
                 },
-                {   
+                {
                     name: 'Boxer dài',
                     link: `/collection/1`
                 }
@@ -114,30 +114,31 @@ function Header() {
                     alt: 'Jeans Copper Denim',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/August2022/v2navytruoc_78.jpg',
                     alt: 'Quần lửng',
                 },
                 {
-                    src:  'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
+                    src: 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/March2023/DSC04764_copy_35.jpg',
                     alt: 'Quần dài',
                 }
             ]
         }
     ]
+
     const handleMouseOver = (index) => {
         setIndexCategoryActive(index)
         setShowDetailPopup(true)
     }
     return (
 
-            
+
         <div className={cx('wrapper')}>
-            <a href='/user'  className={cx('logo-wrapper')}>
+            <a href='/user' className={cx('logo-wrapper')}>
                 <div>SHOP</div>
                 <div className={cx('child2')}>APP</div>
             </a>
 
-            <div className={cx('categories')}>
+            <div onMouseLeave={() => setShowDetailPopup(false)} className={cx('categories')}>
                 {
 
                     <>
@@ -148,13 +149,17 @@ function Header() {
                         {
                             categories.map((item, index) => {
                                 return (
-                                    <div onMouseOver={() => handleMouseOver(index)} key={index} className={cx('item-category') }>
+                                    <div onMouseOver={() => handleMouseOver(index)} key={index} className={cx('item-category')}>
                                         {item.category}
                                     </div>
                                 )
                             })
                         }
                     </>
+                }
+                {
+                    showDetailPopUp &&
+                    <DetailPopup category={categories[indexCategoryActive]} onMouseLeave={() => setShowDetailPopup(false)} />
                 }
             </div>
 
@@ -163,8 +168,8 @@ function Header() {
                     <IoSearch style={{ color: '#000', fontSize: '24px' }} />
                 </div>
                 <div className={cx('input-wrapper')}>
-                    <input onChange={(e) => setValueSearch(e.target.value.trim())} className={cx('input-search')} type='text' value={valueSearch} placeholder='Tìm kiếm sản phẩm...'/>
-                    <div className={cx('line')}></div>
+                    <input onChange={(e) => setValueSearch(e.target.value.trim())} className={cx('input-search')} type='text' value={valueSearch} placeholder='Tìm kiếm sản phẩm...' />
+
                 </div>
                 <div onClick={() => setValueSearch('')} style={{ padding: '0 12px', cursor: 'pointer' }}>
                     <IoMdClose style={{ color: '#000', fontSize: '24px' }} />
@@ -173,17 +178,14 @@ function Header() {
             </div>
 
             <div className={cx('user')}>
-                <div style={{cursor: 'pointer'}}><img src='https://mcdn.coolmate.me/image/October2023/mceclip3_72.png' /></div>
-                <div style={{cursor: 'pointer'}}><IoBagHandle style={{ color: '#fff', fontSize: '28px' }} /></div>
+                <div style={{ cursor: 'pointer' }}><img src='https://mcdn.coolmate.me/image/October2023/mceclip3_72.png' /></div>
+                <div style={{ cursor: 'pointer' }}><IoBagHandle style={{ color: '#fff', fontSize: '28px' }} /></div>
             </div>
-            {
-                showDetailPopUp && 
-                <DetailPopup category={categories[indexCategoryActive]} onMouseLeave={() => setShowDetailPopup(false)} />
-            }
-       
+
+
         </div>
-   
-  
+
+
 
 
     );
--- shop-app-client/src/pages/user/Collection/index.js
+++ shop-app-client/src/pages/user/Collection/index.js
@@ -1,16 +1,249 @@
 import classNames from "classnames/bind";
 import styles from './Collection.module.scss'
+import { useEffect, useState } from "react";
 
 const cx = classNames.bind(styles)
 function Collection() {
+    const [category, setCategory] = useState(() => {
+        return {
+            category: 'Áo',
+            type: [
+                {
+                    name: 'Áo khoác',
+                    checked: false
+                },
+                {
+                    name: 'Áo mùa đông',
+                    checked: false
+                },
+                {
+                    name: 'Áo thể thao',
+                    checked: false
+                },
+                {
+                    name: 'Áo tết',
+                    checked: false
+                },
+            ],
+            size: [
+                {
+                    name: 'S',
+                    checked: false
+                },
+                {
+                    name: 'M',
+                    checked: false
+                },
+                {
+                    name: 'L',
+                    checked: false
+                },
+                {
+                    name: 'XL',
+                    checked: false
+                },
+                {
+                    name: '2XL',
+                    checked: false
+                },
+                {
+                    name: '3XL',
+                    checked: false
+                }
+            ],
+            color: [
+                {
+                    colorName: 'Đen',
+                    colorCode: '#000'
+                },
+                {
+                    colorName: 'Xám',
+                    colorCode: '#939393'
+                },
+                {
+                    colorName: 'Xám',
+                    colorCode: '#152e6e'
+                },
+                {
+                    colorName: 'Trắng',
+                    colorCode: '#f6f6f6'
+                },
+
+            ]
+        }
+    })
+    const [conditions, setConditions] = useState({
+        size: [],
+        color: '',
+        type: []
+    })
+    const [condititonsSelected, setCondititonsSelected] = useState([]);
+    const handleClickType = (type, index) => {
+        let state;
+        setCategory(prev => {
+            state = {
+                ...prev, type: prev.type.map((item, index2) => {
+                    if (index2 === index) return { ...item, checked: !item.checked };
+                    else return item;
+                })
+            }
+            return state;
+        })
+        setConditions(prev => {
+            const newType = state.type.map(item => {
+                if (item.checked) return item.name;
+                else return '';
+            }).filter(item => item !== '');
+            const nextState = { ...prev, type: newType };
+            return nextState;
+        })
+
+    }
+    const handleClickSize = (size, index) => {
+        let state;
+        setCategory(prev => {
+            state = {
+                ...prev, size: prev.size.map((item, index2) => {
+                    if (index2 === index) return { ...item, checked: !item.checked };
+                    else return item;
+                })
+            }
+            return state;
+        })
+        setConditions(prev => {
+            const newSize = state.size.map(item => {
+                if (item.checked) return item.name;
+                else return '';
+            }).filter(item => item !== '');
+            const nextState = { ...prev, size: newSize };
+            return nextState;
+        })
+
+    }
+    const handleClickColor = (color) => {
+        setConditions(prev => {
+            return { ...prev, color: color };
+        })
+    }
+    const handleClickRemoveSelected = (item) => {
+        const key = item.key;
+        const name = item.name;
+        setConditions(prev => {
+            const nextState = { ...prev };
+            if (key === 'color') {
+                nextState.color = '';
+            }
+            else {
+                nextState[key] = [...nextState[key]].filter(item => item !== name);
+            }
+            return nextState;
+        })
+        if (key !== 'color') {
+            setCategory(prev => {
+                const nextState = { ...prev };
+                nextState[key] = [...nextState[key]].map((item) => {
+                    if (item.name === name) {
+                        return { ...item, checked: false }
+                    }
+                    else return item;
+                })
+                return nextState;
+
+            })
+        }
+
+
+    }
+    useEffect(() => {
+        setCondititonsSelected(prev => {
+            let nextState = [];
+            for (var key in conditions) {
+                const value = conditions[key];
+                if (key !== 'color') {
+                    const add = value.map(item => ({ name: item, key }));
+                    nextState = nextState.concat(add)
+                }
+                else {
+                    if (value) nextState.push({ name: value, key });
+                }
+            }
+            console.log(nextState);
+            return nextState;
+        })
+    }, [conditions])
+
     return (
         <div className={cx('wrapper')}>
-            collection
             <div className={cx('left-side')}>
+                <div className={cx('quantity')}>
+                    21 kết quả
+                </div>
+                <div className={cx('filter')}>
+                    <div className={cx('filter-item')}>
+                        <h5 className={cx('filter-heading')}>Kích cỡ</h5>
+                        <ul className={cx('filter-size')}>
+                            {
+                                category.size.map((item, index) => {
+                                    return <li key={index} style={{ position: 'relative', marginTop: '8px' }}>
+                                        <div onClick={() => handleClickSize(item.name, index)} className={cx('filter-size-item', { checked: item.checked })}>
+                                            {item.name}
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className={cx('filter-item')}>
+                        <h5 className={cx('filter-heading')}>Màu sắc</h5>
+                        <ul className={cx('filter-color')}>
+                            {
+                                category.color.map((item, index) => {
+                                    return <li key={index} style={{ position: 'relative', marginTop: '8px' }}>
+                                        <div className={cx('filter-color-item')}>
+                                            <div onClick={() => handleClickColor(item.colorName)} className={cx('filter-select-color__button')} style={{ backgroundColor: `${item.colorCode}` }}>
 
+                                            </div>
+                                            <label className={cx('filter-select-color__label')}>{item.colorName}</label>
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className={cx('filter-item')}>
+                        <h5 className={cx('filter-heading')} style={{ marginBottom: '16px' }}>Loại sản phẩm</h5>
+                        <ul className={cx('filter-type')}>
+                            {
+                                category.type.map((item, index) => {
+                                    return <li key={index} style={{ position: 'relative', marginBottom: '6px', cursor: 'pointer' }}>
+                                        <div onClick={() => handleClickType(item.name, index)} className={cx('filter-type-item')}>
+                                            <div className={cx('filter-type-checkbox', { checked: item.checked })}></div>
+                                            <label className={cx('filter-type-label')}>{item.name}</label>
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className={cx('right-side')}>
+                <h1 className={cx('heading')}>Áo</h1>
+                <div className={cx('selected')}>
+                    <h5>16 kết quả</h5>
 
+                    <div className={'wrapper-item-selected'}>
+                        {
+                            condititonsSelected.map((item, index) => {
+                                return <span className={cx('item-selected')} key={index}>{item.name}
+                                    <span onClick={() => handleClickRemoveSelected(item)}
+                                        style={{ fontSize: '12px', marginLeft: '5px', opacity: '1', fontWeight: '600', cursor: 'pointer', padding: '0 2px' }}>x</span></span>
+                            })
+                        }
+                    </div>
+
+
+                </div>
             </div>
 
         </div>
