2227b0c73b01b9016cc100cea3bf4bdc32f7c009
Fatitboo <21522448@gm.uit.edu.vn>
2023-11-23 22:00:02 +0700

    merge 2311

--- shop-app-client/src/pages/admin/Dashboard/index.js
+++ shop-app-client/src/pages/admin/Dashboard/index.js
@@ -107,205 +107,207 @@ function Dashboard() {
           <div >
             {/* Lọc theo thời gian */}
             <div className={cx("container-1")}>
-            <div className={cx("container-filter")}>
-          <span>Chọn năm: </span>
-          <select
-            className={cx("cbb-filter", "form-select")}
-            aria-label="Default select example"
-          >
-            <option selected value="All">
-              Tất cả
-            </option>
-            <option value="2020">Năm 2020</option>
-            <option value="2021">Năm 2021</option>
-            <option value="2023">Năm 2023</option>
-          </select>
-          <span>Chọn tháng: </span>
-          <select
-            className={cx("cbb-filter", "form-select")}
-            aria-label="Default select example"
-          >
-            <option selected value="All">
-              Tất cả
-            </option>
-            <option value="1">Tháng 1</option>
-            <option value="2">Tháng 2</option>
-            <option value="3">Tháng 3</option>
-            <option value="4">Tháng 4</option>
-            <option value="5">Tháng 5</option>
-            <option value="6">Tháng 6</option>
-            <option value="7">Tháng 7</option>
-            <option value="8">Tháng 8</option>
-            <option value="9">Tháng 3</option>
-            <option value="10">Tháng 10</option>
-            <option value="11">Tháng 11</option>
-            <option value="12">Tháng 12</option>
-          </select>
-        </div>
-      </div>
-      {/* Tổng Doanh Thu, Tổng nhập hàng, Số đơn hàng, Số Khách hàng  */}
-      <div className={cx('container-total')}>
-        <span className={cx('title-total')}>So với năm/tháng trước đó</span>
-        <div className={cx('container-item-board')}>
-          <ItemDashboard
-            title="DOANH THU (VND)"
-            percent={27.96}
-            value={formatMoney(14276000)}
-            isIncrease={true}
-          />
-          <ItemDashboard
-            title="NHẬP HÀNG (VND)"
-            percent={52.96}
-            value={formatMoney(12476000)}
-            isIncrease={false}
-          />
-          <ItemDashboard
-            title="SỐ ĐƠN HÀNG"
-            percent={17.96}
-            value={149}
-            isIncrease={false}
-          />
-          <ItemDashboard
-            title="SỐ KHÁCH HÀNG"
-            percent={28.96}
-            value={212}
-            isIncrease={true}
-          />
-        </div>
-      </div>
+              <div className={cx("container-filter")}>
+                <span>Chọn năm: </span>
+                <select
+                  className={cx("cbb-filter", "form-select")}
+                  aria-label="Default select example"
+                >
+                  <option selected value="All">
+                    Tất cả
+                  </option>
+                  <option value="2020">Năm 2020</option>
+                  <option value="2021">Năm 2021</option>
+                  <option value="2023">Năm 2023</option>
+                </select>
+                <span>Chọn tháng: </span>
+                <select
+                  className={cx("cbb-filter", "form-select")}
+                  aria-label="Default select example"
+                >
+                  <option selected value="All">
+                    Tất cả
+                  </option>
+                  <option value="1">Tháng 1</option>
+                  <option value="2">Tháng 2</option>
+                  <option value="3">Tháng 3</option>
+                  <option value="4">Tháng 4</option>
+                  <option value="5">Tháng 5</option>
+                  <option value="6">Tháng 6</option>
+                  <option value="7">Tháng 7</option>
+                  <option value="8">Tháng 8</option>
+                  <option value="9">Tháng 3</option>
+                  <option value="10">Tháng 10</option>
+                  <option value="11">Tháng 11</option>
+                  <option value="12">Tháng 12</option>
+                </select>
+              </div>
+            </div>
+            {/* Tổng Doanh Thu, Tổng nhập hàng, Số đơn hàng, Số Khách hàng  */}
+            <div className={cx('container-total')}>
+              <span className={cx('title-total')}>So với năm/tháng trước đó</span>
+              <div className={cx('container-item-board')}>
+                <ItemDashboard
+                  title="DOANH THU (VND)"
+                  percent={27.96}
+                  value={formatMoney(14276000)}
+                  isIncrease={true}
+                />
+                <ItemDashboard
+                  title="NHẬP HÀNG (VND)"
+                  percent={52.96}
+                  value={formatMoney(12476000)}
+                  isIncrease={false}
+                />
+                <ItemDashboard
+                  title="SỐ ĐƠN HÀNG"
+                  percent={17.96}
+                  value={149}
+                  isIncrease={false}
+                />
+                <ItemDashboard
+                  title="SỐ KHÁCH HÀNG"
+                  percent={28.96}
+                  value={212}
+                  isIncrease={true}
+                />
+              </div>
+            </div>
 
-      <div className={cx('container-list-chart')}>
-        <div style={{ width: "70%", padding: "20px" }}>
-          <div className={cx('container-big-chart')}>
-            <span style={{ fontSize: "18px", fontWeight: "500" }}>
-              Tổng thu: 29.850.000 (VND)
-            </span>
-            <span style={{ fontSize: "18px", fontWeight: "500" }}>
-              Tổng chi: 29.850.000 (VND)
-            </span>
-          </div>
-          <Bar
-            data={data_Thu_Chi}
-            width={null}
-            height={null}
-            options={options}
-          />
-        </div>
-
-        <div style={{ width: "30%", padding: "20px" }}>
-          <Doughnut data={dataProductType} />
-        </div>
-      </div>
-
-      <div className={cx('container-two-table')}>
-        <div className={cx('bg-table-small')}>
-          <span className={cx('title-table')}>
-            Top 5 sản phẩm bán chạy
-          </span>
-          <div className={cx('container-header-table')}>
-            <span>Hình ảnh</span>
-            <span style={{ marginLeft: '10px'}}>Tên sản phẩm</span>
-            <span style={{marginLeft: '18px'}}>Giá bán</span>
-            <span style={{marginRight: '10px'}}>Đã bán</span>
-            <span>Doanh số</span>
-          </div>
-          <div className={cx('layout-col')}>
-            {[1, 2, 3, 4, 5].map(() => {
-              return (
-                <div className={cx('container-item-table')}>
-                  <img className={cx('img-item-table')} src={item_product} alt="img"/>
-                  <div>
-                    <p className={cx('three-dot-text')}>
-                      Branded T-Shirts
-                    </p>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                    <p className={cx('text-rating')}>(4.5)</p>
-                    <ReactStars
-                        count={5}
-                        size={10}
-                        color1={"#ccc"}
-                        color2={"#ffd700"}
-                        edit={false}
-                        value={4.5}
-                      />
-                    </div>
-                  </div>
-                  <p>{formatMoney(149000)}</p>
-                  <p>162</p>
-                  <p>{formatMoney(2000000)}</p>
+            <div className={cx('container-list-chart')}>
+              <div style={{ width: "70%", padding: "20px" }}>
+                <div className={cx('container-big-chart')}>
+                  <span style={{ fontSize: "18px", fontWeight: "500" }}>
+                    Tổng thu: 29.850.000 (VND)
+                  </span>
+                  <span style={{ fontSize: "18px", fontWeight: "500" }}>
+                    Tổng chi: 29.850.000 (VND)
+                  </span>
                 </div>
-              );
-            })}
-          </div>
-        </div>
+                <Bar
+                  data={data_Thu_Chi}
+                  width={null}
+                  height={null}
+                  options={options}
+                />
+              </div>
 
-        <div className={cx('bg-table-small')}>
-          <span className={cx('title-table')}>
-            Top khách hàng chi tiêu
-          </span>
-          <div className={cx('container-header-table')}>
-            <span>Hình ảnh</span>
-            <span style={{marginLeft: '10px'}}>Họ tên</span>
-            <span style={{marginLeft: '20px'}}>Số điện thoại</span>
-            <span>Địa chỉ email</span>
-            <span>Mức chi tiêu</span>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            {[1, 2, 3, 4, 5].map(() => {
-              return (
-                <div className={cx('container-item-table')}>
-                  <img className={cx('img-item-table')} src={item_product} alt="img"/>
-                  <p className={cx('three-dot-text')}>Nguyễn Văn A</p>
-                  <p>0379361210</p>
-                  <p className={cx('three-dot-text')}>tinh@gmail.com</p>
-                  <p>1.999.000 đ</p>
+              <div style={{ width: "30%", padding: "20px" }}>
+                <Doughnut data={dataProductType} />
+              </div>
+            </div>
+
+            <div className={cx('container-two-table')}>
+              <div className={cx('bg-table-small')}>
+                <span className={cx('title-table')}>
+                  Top 5 sản phẩm bán chạy
+                </span>
+                <div className={cx('container-header-table')}>
+                  <span>Hình ảnh</span>
+                  <span style={{ marginLeft: '10px' }}>Tên sản phẩm</span>
+                  <span style={{ marginLeft: '18px' }}>Giá bán</span>
+                  <span style={{ marginRight: '10px' }}>Đã bán</span>
+                  <span>Doanh số</span>
                 </div>
-              );
-            })}
+                <div className={cx('layout-col')}>
+                  {[1, 2, 3, 4, 5].map(() => {
+                    return (
+                      <div className={cx('container-item-table')}>
+                        <img className={cx('img-item-table')} src={item_product} alt="img" />
+                        <div>
+                          <p className={cx('three-dot-text')}>
+                            Branded T-Shirts
+                          </p>
+                          <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <p className={cx('text-rating')}>(4.5)</p>
+                            <ReactStars
+                              count={5}
+                              size={10}
+                              color1={"#ccc"}
+                              color2={"#ffd700"}
+                              edit={false}
+                              value={4.5}
+                            />
+                          </div>
+                        </div>
+                        <p>{formatMoney(149000)}</p>
+                        <p>162</p>
+                        <p>{formatMoney(2000000)}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div className={cx('bg-table-small')}>
+                <span className={cx('title-table')}>
+                  Top khách hàng chi tiêu
+                </span>
+                <div className={cx('container-header-table')}>
+                  <span>Hình ảnh</span>
+                  <span style={{ marginLeft: '10px' }}>Họ tên</span>
+                  <span style={{ marginLeft: '20px' }}>Số điện thoại</span>
+                  <span>Địa chỉ email</span>
+                  <span>Mức chi tiêu</span>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  {[1, 2, 3, 4, 5].map(() => {
+                    return (
+                      <div className={cx('container-item-table')}>
+                        <img className={cx('img-item-table')} src={item_product} alt="img" />
+                        <p className={cx('three-dot-text')}>Nguyễn Văn A</p>
+                        <p>0379361210</p>
+                        <p className={cx('three-dot-text')}>tinh@gmail.com</p>
+                        <p>1.999.000 đ</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            {/* List table đơn hàng gần đây  */}
+            <div style={{ margin: "20px 30px" }}>
+              <div className={cx('border-table')}>
+                <span className={cx('title-table')}>
+                  Những đơn hàng gần đây
+                </span>
+                <div className={cx('container-header-table')}>
+                  <span style={{ marginLeft: '20px' }}>Hình ảnh</span>
+                  <span style={{ marginLeft: '15px' }}>Mã đơn hàng</span>
+                  <span style={{ marginRight: '20px' }}>Mã sản phẩm</span>
+                  <span style={{ marginRight: '60px' }}>Tên sản phẩm</span>
+                  <span style={{ marginRight: '40px' }}>Tên khách hàng</span>
+                  <span style={{ marginRight: '25px' }}>Số điện thoại</span>
+                  <span style={{ marginRight: '20px' }}>Giá trị đơn hàng</span>
+                </div>
+                <div className={cx('container-order-recent')}>
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7].map(() => {
+                    return (
+                      <div className={cx('container-item-table')}>
+                        <img className={cx('img-item-table')} src={item_product} alt="img" />
+                        <p>DH001</p>
+                        <p>SP001</p>
+                        <p className={cx('three-dot-text')}>Branded T-Shirts</p>
+                        <p className={cx('three-dot-text')}>Phan Trọng Tính</p>
+                        <p>0379361210</p>
+                        <p>2.000.999 đ</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* @2023 copyright */}
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <span style={{ textAlign: 'center', margin: '20px 0 30px' }}>2023 @copyright</span>
           </div>
         </div>
-      </div>
-      {/* List table đơn hàng gần đây  */}
-      <div style={{ margin: "20px 30px" }}>
-        <div className={cx('border-table')}>
-          <span className={cx('title-table')}>
-            Những đơn hàng gần đây
-          </span>
-          <div className={cx('container-header-table')}>
-            <span style={{marginLeft: '20px'}}>Hình ảnh</span>
-            <span style={{marginLeft: '15px'}}>Mã đơn hàng</span>
-            <span style={{marginRight: '20px'}}>Mã sản phẩm</span>
-            <span style={{marginRight: '60px'}}>Tên sản phẩm</span>
-            <span style={{marginRight: '40px'}}>Tên khách hàng</span>
-            <span style={{marginRight: '25px'}}>Số điện thoại</span>
-            <span style={{marginRight: '20px'}}>Giá trị đơn hàng</span>
-          </div>
-          <div className={cx('container-order-recent')}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7].map(() => {
-              return (
-                <div className={cx('container-item-table')}>
-                  <img className={cx('img-item-table')} src={item_product} alt="img"/>
-                  <p>DH001</p>
-                  <p>SP001</p>
-                  <p className={cx('three-dot-text')}>Branded T-Shirts</p>
-                  <p className={cx('three-dot-text')}>Phan Trọng Tính</p>
-                  <p>0379361210</p>
-                  <p>2.000.999 đ</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-      </div>
-      {/* @2023 copyright */}
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <span style={{ textAlign: 'center', margin: '20px 0 30px' }}>2023 @copyright</span>
       </div>
     </div>
   );
