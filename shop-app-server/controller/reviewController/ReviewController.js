cacc05b1eeca672cdf6fe70f3f2556e24adb61d7
21522683 <21522683@gm.uit.edu.vn>
2023-12-03 10:24:17 +0700

    .

--- shop-app-server/controller/reviewController/ReviewController.js
+++ shop-app-server/controller/reviewController/ReviewController.js
@@ -1,28 +1,39 @@
-const Review = require("../../model/review/Review")
-const successTemplate = require('../../templates/succesTemplate');
+const Review = require("../../model/review/Review");
+const successTemplate = require("../../templates/succesTemplate");
 const errorTemplate = require("../../templates/errorTemplate");
 
-
 const getAllReviews = async (req, res) => {
-    try {
-        const reviews = await Review.find({}).populate({path: "orderItem"}).exec();
-        
-        return successTemplate(res, reviews, "Get all reviews successfully!", 200)
-    } catch (error) {
-        return errorTemplate(res, error.message)
-    }
-}
+  try {
+    const reviews = await Review.find({})
+      .populate([
+        {
+          path: "user", 
+          model: "User"
+        }, 
+        { 
+          path: "orderItem", 
+          model: "OrderItem", 
+          populate: {
+            path: "productId", 
+            model: "Product"
+          }
+        }
+      ])
+      .exec();
+    return successTemplate(res, reviews, "Get all reviews successfully!", 200);
+  } catch (error) {
+    return errorTemplate(res, error.message);
+  }
+};
 const responseReview = async (req, res) => {
-    try {
-        
-        
-        return successTemplate(res, review, "Responsed review successfully!", 200)
-    } catch (error) {
-        return errorTemplate(res, error.message)
-    }
-}
+  try {
+    return successTemplate(res, review, "Responsed review successfully!", 200);
+  } catch (error) {
+    return errorTemplate(res, error.message);
+  }
+};
 
 module.exports = {
-    getAllReviews,
-    responseReview,
-}
+  getAllReviews,
+  responseReview,
+};
